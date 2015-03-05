b0624d2e9f4472f142122632389139d1d9d7ca26
sebastian.marschall.ft <sebastian.marschall@fluidtime.com>
2015-03-05 14:33:01 +0100

    ~ added backwards compatibility

--- NanoTasks/nanotasks/src/main/java/com/nanotasks/Tasks.java
+++ NanoTasks/nanotasks/src/main/java/com/nanotasks/Tasks.java
@@ -2,6 +2,7 @@ package com.nanotasks;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Build;
 
 import java.util.concurrent.Executor;
 
@@ -13,7 +14,11 @@ public final class Tasks {
     private Tasks() { throw new UnsupportedOperationException(); }
 
     public static <T> void executeInBackground(Context context, BackgroundWork<T> backgroundWork, Completion<T> completion) {
-        new Task<T>(context, backgroundWork, completion).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            new Task<T>(context, backgroundWork, completion).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        } else {
+            new Task<T>(context, backgroundWork, completion).execute();
+        }
     }
 
     public static <T> void executeInBackground(Context context, BackgroundWork<T> backgroundWork, Completion<T> completion, Executor executor) {
